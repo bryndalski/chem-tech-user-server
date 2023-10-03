8dffe97a3186f81b2094b82a25db3b2fbc49d2cc
bryndalski <kubabryndal@gmail.com>
2023-10-03 19:08:29 +0200

    setup validation

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe()); // Validation
+  app.enableVersioning({
+    type: VersioningType.URI,
+    prefix: 'v',
+  });
   await app.listen(process.env.PORT);
 }
 void bootstrap();
